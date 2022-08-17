280fa996dc525e0b286a1d9db5bc514f1b9252ba
Karan Tyagi <karantyagi9702@gmail.com>
2022-08-17 20:35:55 +0530

    Setup updateFile route

---
+++ pages/api/updateFile.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import File from "../../models/File";
+import dbConnect from "../../lib/dbConnect";
+
+dbConnect();
+
+export default async function updateFile(req, res) {
+	console.log("Received update file request");
+	try {
+		const data = jwt.verify(req.headers.token, process.env.SECRET_KEY);
+
+		req.body.userId = data?.id;
+		if (!req.body.userId)
+			throw { status: 400, message: "Please login to continue" };
+
+		const filter = { _id: req.body.fileId, authorId: req.body.userId };
+		const update = {
+			name: req.body.name,
+			lang: req.body.lang,
+			content: req.body.content,
+		};
+
+		const file = await File.findOneAndUpdate(filter, update, { new: true });
+
+		if (!file) throw { status: 404, message: "File not found" };
+
+		res.status(201).json({ success: true, file });
+	} catch (err) {
+		res.status(err.status || 500).json({
+			success: false,
+			message: err.message || "Something went wrong",
+		});
+	}
+}
